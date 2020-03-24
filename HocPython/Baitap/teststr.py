728ffb501c8d10128c72ba67a97f08ebe92f1db8
Veiler <linhnv110196@gmail.com>
2020-03-24 20:37:10 +0700

    add join and split example

--- HocPython/Baitap/teststr.py
+++ HocPython/Baitap/teststr.py
@@ -1,3 +1,14 @@
-inputString = str(input("Hay nhap chuoi ky tu ma ban thich: "))
-result = inputString.replace("abc", "def")
-print(result)
+# inputString = str(input("Hay nhap chuoi ky tu ma ban thich: "))
+# result = inputString.replace("abc", "def")
+# print(result)
+
+# inputString = str(input("Hay nhap chuoi ky tu ma ban thich: "))
+# result = inputString.replace(" ", "\n")
+# print (result)
+
+# result = inputString.split(" ")
+# print(result)
+
+test = ["1", "2", "3", "4"]
+result = ","
+print(result.join(test))
