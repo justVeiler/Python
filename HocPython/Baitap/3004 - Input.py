ae09be48310238ae905a4d88c39da0297110a22c
Veiler <linhnv110196@gmail.com>
2020-04-30 15:51:00 +0700

    Start all over again

---
+++ HocPython/Baitap/3004 - Input.py
@@ -0,0 +1,14 @@
+# Bai tap Nhap ten tuoi & so sanh
+# maxAge = 0
+# maxName = ""
+# for i in range(3):
+#     inputName = str(input("Hay nhap ten nguoi thu {}: ".format(i + 1)))
+#     inputAge = int(input("Hay nhap so tuoi cua nguoi do: "))
+#     if inputAge > maxAge:
+#         maxAge = inputAge
+#         maxName = inputName
+#
+# print(maxName)
+# print(maxAge)
+
+#
